a20b3056461237ee718a589642779c4550a9e503
LIUJIAYI <liujiayii@foxmail.com>
2020-05-08 23:10:23 +0800

    配置自动copy assets目录

--- remax.config.js
+++ remax.config.js
@@ -1,3 +1,9 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   pxToRpx: false,
-};
+  configWebpack: ({ config }) => {
+    // 详细配置参考 copy-webpack-plugin
+    config.plugin('copy').use(CopyPlugin, [[{ from: 'src/assets', to: 'assets' }]])
+  }
+}
