39bfc8457a7bcabdfacd03d03a5a3d51e0d3079d
weber <liujiayii@foxmail.com>
2020-06-11 23:18:16 +0800

    feat:use public directory

--- remax.config.js
+++ remax.config.js
@@ -1,10 +1,6 @@
-//const CopyPlugin = require('copy-webpack-plugin')
-
 module.exports = {
   pxToRpx: false,
-  configWebpack: ({ config, addCSSRule }) => {
-    // 详细配置参考 copy-webpack-plugin
-    //  config.plugin('copy').use(CopyPlugin, [[{ from: 'src/assets', to: 'assets' }]])
+  configWebpack: ({ addCSSRule }) => {
     // 引用 wxss
     addCSSRule({
       name: 'wxss',
